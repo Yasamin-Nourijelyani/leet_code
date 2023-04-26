8d44d0f24b745d2d88552db0b4077708c4c6f6e0
Yasamin Nouri Jelyani <yasamin.nourijelyani@mail.utoronto.ca>
2023-04-26 12:00:21 -0400

    added new files

---
+++ main.py
@@ -0,0 +1,8 @@
+import unique_emails
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+    unique_emails.num_emails(emails)
+
---
+++ unique_emails.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def num_emails(emails: List[str]) -> int:
+    """Return the number of emails that are unique
+    
+    :emails: The list of emails that we want to see which are unique
+
+    Example usage: 
+
+    >>> emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+    >>> num_emails(emails)
+    2
+
+    >>> emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+    >>> num_emails(emails)
+    3
+    """
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {emails}')  # Press Ctrl+F8 to toggle the breakpoint.
